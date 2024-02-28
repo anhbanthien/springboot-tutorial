1c602ce3d2e7118d829fbdb57ce06ab214da10a5
anhbanthien <thienncph22476@fpt.edu.vn>
2024-02-28 16:42:35 +0700

    cachePut and cacheEvict

--- src/main/java/org/example/nobs/command/commandhandler/UpdateProductCommand.java
+++ src/main/java/org/example/nobs/command/commandhandler/UpdateProductCommand.java
@@ -5,6 +5,8 @@ import org.example.nobs.command.productupdate.ProductUpdate;
 import org.example.nobs.entity.Product;
 import org.example.nobs.repo.ProductRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +17,8 @@ public class UpdateProductCommand implements Command<ProductUpdate,ResponseEntit
     private ProductRepo productRepo;
 
     @Override
+    //@CacheEvict(value = "productCache",key = "#productUpdate.getId()")
+    @CachePut(value = "productCache",key = "#productUpdate.getId()")
     public ResponseEntity<ResponseEntity> execute(ProductUpdate productUpdate) {
         Product product = productUpdate.getProduct();
         product.setId(productUpdate.getId());
--- src/main/java/org/example/nobs/query/queryhandler/GetProductQueryHandler.java
+++ src/main/java/org/example/nobs/query/queryhandler/GetProductQueryHandler.java
@@ -5,6 +5,7 @@ import org.example.nobs.entity.Product;
 import org.example.nobs.query.Query;
 import org.example.nobs.repo.ProductRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
@@ -16,6 +17,7 @@ public class GetProductQueryHandler implements Query<Integer, ProductDto> {
     @Autowired
     private ProductRepo productRepo;
     @Override
+    @Cacheable("productCache")
     public ResponseEntity<ProductDto> execute(Integer input) {
         Optional<Product> product = productRepo.findById(input);
         ProductDto productDto = new ProductDto(product.get());
