c62de8de9304e3c6fdb102cb14f971c3adc2c397
anhbanthien <thienncph22476@fpt.edu.vn>
2024-03-01 15:13:14 +0700

    Method Level Security @PreAuthorize

--- src/main/java/org/example/nobs/security/SecurityConfiguration.java
+++ src/main/java/org/example/nobs/security/SecurityConfiguration.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -14,6 +16,7 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfiguration {
     @Bean
     public UserDetailsService userDetailsService(PasswordEncoder encoder){
@@ -38,11 +41,22 @@ public class SecurityConfiguration {
     public SecurityFilterChain securityFilterChain (HttpSecurity httpSecurity) throws Exception {
             return  httpSecurity
                     .authorizeRequests()
-                    .requestMatchers(HttpMethod.GET,"special").hasRole("SPECIAL")
-                    .requestMatchers(HttpMethod.GET,"basic").hasRole("SPECIAL,BASIC")
+                    .anyRequest()
+                    .authenticated()
+//                    .requestMatchers(HttpMethod.GET,"special").hasRole("SPECIAL")
+//                    .requestMatchers(HttpMethod.GET,"basic").hasRole("SPECIAL,BASIC")
                     .and()
                     .formLogin(Customizer.withDefaults())
                     .httpBasic(Customizer.withDefaults())
                     .build();
     }
+
+    //note: trong đoạn mã này tôi đã sử dụng @EnableGlobalMethodSecurity(prePostEnabled = true)
+    // để kích hoạt việc sử dụng các Annotation bảo mật trên phương thức như @PreAuthorize.
+    //luồng hoạt động của nó sẽ là :
+    //Khi một yêu cầu đến endpoint /special, nó sẽ đi qua phương thức special() được đánh dấu bằng @PreAuthorize("hasRole('ADMIN')").
+    // Điều này có nghĩa là chỉ người dùng có vai trò ADMIN mới có thể truy cập vào endpoint này. Vì vậy, nếu người dùng không có vai trò ADMIN, yêu cầu sẽ bị từ chối và trả về mã trạng thái 403 Forbidden.
+    //Khi một yêu cầu đến endpoint /basic, nó sẽ đi qua phương thức basic() được đánh dấu bằng @PreAuthorize("hasRole('ADMIN') or hasRole('USER')").
+    // Điều này có nghĩa là người dùng cần có vai trò ADMIN hoặc USER mới có thể truy cập vào endpoint này. Vì vậy, nếu người dùng không có vai trò ADMIN hoặc USER, yêu cầu sẽ bị từ chối và trả về mã trạng thái 403 Forbidden.
+    //Ngoài ra, tất cả các yêu cầu khác đều sẽ yêu cầu người dùng phải xác thực (authenticated()) thông qua cơ chế đăng nhập (form login hoặc HTTP Basic). Điều này đảm bảo rằng các phần của ứng dụng không được bảo vệ bởi các quy tắc ủy quyền cụ thể vẫn yêu cầu người dùng phải đăng nhập để truy cập.
 }
--- src/main/java/org/example/nobs/security/SecurityController.java
+++ src/main/java/org/example/nobs/security/SecurityController.java
@@ -1,23 +1,26 @@
 package org.example.nobs.security;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SecurityController {
-    @GetMapping("/open")
-    public String open (){
-        return "No login Required";
-    }
-    @GetMapping("/closed")
-    public String closed (){
-        return "Login IS Required";
-    }
+//    @GetMapping("/open")
+//    public String open (){
+//        return "No login Required";
+//    }
+//    @GetMapping("/closed")
+//    public String closed (){
+//        return "Login IS Required";
+//    }
 
+    @PreAuthorize("hasRole('SPECIAL')")
     @GetMapping("/special")
     public String special (){
         return "SPECIAL";
     }
+    @PreAuthorize("hasRole('SPECIAL') or hasRole('BASIC')")
     @GetMapping("/basic")
     public String basic (){
         return "BASIC";
